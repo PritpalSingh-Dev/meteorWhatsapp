fd41ba89a2c3f5401aa246e31c3b29a251475d42
psbadewal <psbadewal@icloud.com>
2016-01-27 23:27:37 +0000

    Corrected errors in routes and ChatCtrl

--- whatsapp/client/scripts/controllers/chat.controller.js
+++ whatsapp/client/scripts/controllers/chat.controller.js
@@ -1,18 +1,15 @@
 angular
   .module('Whatsapp')
   .controller('ChatCtrl', ChatCtrl);
-
-  function ChatCtrl ($scope, $reactive, $stateParams) {
-    $reactive(this).attach($scope);
-
-    let chatId = $stateParams.chatId;
-
-    this.helpers({
-      messages() {
-        return Messages.find(chatId: chatId);
-        data() {
-          return Chats.findOne(chatId);
-        }
-      }
-    })
-  }
+ 
+function ChatCtrl ($scope, $reactive, $stateParams) {
+  $reactive(this).attach($scope);
+ 
+  let chatId = $stateParams.chatId;
+ 
+  this.helpers({
+    data() {
+      return Chats.findOne(chatId);
+    }
+  });
+}
