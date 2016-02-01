1a3e60c651d4a7a875070678e99b1ba93a36ba76
psbadewal <psbadewal@icloud.com>
2016-02-01 16:09:07 +0000

    Inserting functions inputUp, inputDown and closeKeyboard into chat.Controller.js

--- whatsapp/client/scripts/controllers/chat.controller.js
+++ whatsapp/client/scripts/controllers/chat.controller.js
@@ -2,12 +2,15 @@ angular
 .module('Whatsapp')
 .controller('ChatCtrl', ChatCtrl);
 
-function ChatCtrl ($scope, $reactive, $stateParams) {
+function ChatCtrl ($scope, $reactive, $stateParams, $ionicScrollDelegate, $timeout) {
   $reactive(this).attach($scope);
 
   let chatId = $stateParams.chatId;
+  let isIOS = ionic.Platform.isWebView() && ionic.Platform.isIOS();
   this.sendMessage = sendMessage;
-
+  this.inputUp = inputUp;
+  this.inputDown = inputDown;
+  this.closeKeyboard = closeKeyboard;
   this.helpers({
     messages() {
       return Messages.find({ chatId: chatId });
@@ -22,4 +25,27 @@ function ChatCtrl ($scope, $reactive, $stateParams) {
   function sendMessage() {
   //TODO: implement
   }
+
+  function inputUp() {
+    if (isIOS) {
+      this.keyboardHeight = 216;
+    }
+
+    $timeout(function() {
+      $ionicScrollDelegate.$getByHandle('chatScroll').scrollBottom(true);
+    }, 300);
+  }
+
+  function inputDown(){ 
+    if (isIOS) {
+      this.keyboardHeigh = 0;
+    }
+
+    $ionicScrollDelegate.$getByHandle('chatScroll').resize();
+  }
+
+  function closeKeyboard() {
+    // cordova.plugins.Keyboard.close();
+  }
 }
+
