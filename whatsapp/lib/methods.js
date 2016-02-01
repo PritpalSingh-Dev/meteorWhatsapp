c6bee3bfa9fc3d34124fad981adcde71cdcd25e9
psbadewal <psbadewal@icloud.com>
2016-02-01 17:20:02 +0000

    Corrected error within sendMessage check function

--- whatsapp/lib/methods.js
+++ whatsapp/lib/methods.js
@@ -2,12 +2,13 @@ Meteor.methods({
   newMessage (message) {
     check(message, {
       text: String,
+      type: String,
       chatId: String
     });
 
     message.timestamp = new Date();
 
-    var messageId = Messages.insert(message);
+    let messageId = Messages.insert(message);
     Chats.update(message.chatId, { $set: { lastMessage: message } });
     
     return messageId;
