65c14d8e2f5eef43f0371d5bf32ba36560ec923b
psbadewal <psbadewal@icloud.com>
2016-02-01 16:28:22 +0000

    Changing let to var in newMessage method

--- whatsapp/lib/methods.js
+++ whatsapp/lib/methods.js
@@ -7,7 +7,7 @@ Meteor.methods({
 
     message.timestamp = new Date();
 
-    let messageId = Messages.insert(message);
+    var messageId = Messages.insert(message);
     Chats.update(message.chatId, { $set: { lastMessage: message } });
     return messageId;
   }
