44331874e561f08b6ccb09f48ad49411c1dc4013
psbadewal <psbadewal@icloud.com>
2016-02-20 23:57:13 +0000

    Adding demo app json settings to server

--- whatsapp/server/sms.js
+++ whatsapp/server/sms.js
@@ -3,6 +3,18 @@
 //   ACCOUNT_SID: Meteor.settings.TWILIO.SID,
 //   AUTH_TOKEN: Meteor.settings.TWILIO.TOKEN
 // };
+{
+  "TWILIO": {
+    "FROM": "meteor-whatsapp",
+    "SID": "",
+    "TOKEN": ""
+  },
+  "ACCOUNTS_PHONE": {
+    "ADMIN_NUMBERS": ["123456789", "987654321"],
+    "MASTER_CODE": "1234"
+  }
+}
+
 if (Meteor.isServer) {
   if (Meteor.settings && Meteor.settings.ACCOUNTS_PHONE) {
     Accounts._options.adminPhoneNumbers = Meteor.settings.ACCOUNTS_PHONE.ADMIN_NUMBERS;
