58356e87a9b523dbc049e94a791cd5f33b197efc
psbadewal <psbadewal@icloud.com>
2016-02-22 20:03:17 +0000

    Inserting function login with confirmPopup

--- whatsapp/client/scripts/controllers/login.controller.js
+++ whatsapp/client/scripts/controllers/login.controller.js
@@ -6,4 +6,24 @@ angular
 $reactive(this).attach($scope);
 
 this.login = login;
+
+function login(){
+  if (_.isEmpty(this.phone)) return;
+
+  let confirmPopup = $ionicPopup.confirm({
+    title: 'Number confirmation',
+    template: '<div>' + this.phone + '</div><div>Is this your phone number above correct?</div>',
+    cssClass: 'text-center',
+    okText: 'Yes',
+    okType: 'button-positive button-clear',
+    cancelText: 'edit',
+    cancelType: 'button-dark button-clear'
+  });
+}
+
+
+
+
+
+
   }
