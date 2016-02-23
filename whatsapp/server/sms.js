cb2fc2f2cdf8de742f0c48ce4e1c1748469b7943
psbadewal <psbadewal@icloud.com>
2016-02-23 01:24:29 +0000

    Building the account confirmation view

--- whatsapp/server/sms.js
+++ whatsapp/server/sms.js
@@ -3,17 +3,17 @@
 //   ACCOUNT_SID: Meteor.settings.TWILIO.SID,
 //   AUTH_TOKEN: Meteor.settings.TWILIO.TOKEN
 // };
-{
-  "TWILIO": {
-    "FROM": "meteor-whatsapp",
-    "SID": "",
-    "TOKEN": ""
-  },
-  "ACCOUNTS_PHONE": {
-    "ADMIN_NUMBERS": ["123456789", "987654321"],
-    "MASTER_CODE": "1234"
-  }
-}
+// {
+//   "TWILIO": {
+//     "FROM": "meteor-whatsapp",
+//     "SID": "",
+//     "TOKEN": ""
+//   },
+//   "ACCOUNTS_PHONE": {
+//     "ADMIN_NUMBERS": ["123456789", "987654321"],
+//     "MASTER_CODE": "1234"
+//   }
+// }
 
 if (Meteor.isServer) {
   if (Meteor.settings && Meteor.settings.ACCOUNTS_PHONE) {
