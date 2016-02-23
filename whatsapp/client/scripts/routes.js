06671252dd7adc59d1a3d0fad905f038c106fd83
psbadewal <psbadewal@icloud.com>
2016-02-23 00:04:50 +0000

    Ensuring that loggedIn users can see tabs

--- whatsapp/client/scripts/routes.js
+++ whatsapp/client/scripts/routes.js
@@ -7,7 +7,12 @@ function config($stateProvider, $urlRouterProvider) {
     .state('tab', {
       url: '/tab',
       abstract: true,
-      templateUrl: 'client/templates/tabs.html'
+      templateUrl: 'client/templates/tabs.html',
+      resolve: {
+        user() {
+          return Meteor.user();
+        }
+      }
     })
     .state('tab.chats', {
       url: '/chats',
