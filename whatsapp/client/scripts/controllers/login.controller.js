ad06280106e2277dcb841d54a34241e0b50573cc
psbadewal <psbadewal@icloud.com>
2016-02-23 00:02:32 +0000

    Inserting function handleError

--- whatsapp/client/scripts/controllers/login.controller.js
+++ whatsapp/client/scripts/controllers/login.controller.js
@@ -1,47 +1,51 @@
 angular
-  .module('Whatsapp')
-  .controller('LoginCtrl', LoginCtrl);
+.module('Whatsapp')
+.controller('LoginCtrl', LoginCtrl);
 
-  function LoginCtrl($scope, $reactive, $ionicLoading, $ ionicPopup, $log) {
-$reactive(this).attach($scope);
+function LoginCtrl($scope, $reactive, $ionicLoading, $ ionicPopup, $log) {
+  $reactive(this).attach($scope);
 
-this.login = login;
+  this.login = login;
 
-function login(){
-  if (_.isEmpty(this.phone)) return;
+  function login(){
+    if (_.isEmpty(this.phone)) return;
 
-  let confirmPopup = $ionicPopup.confirm({
-    title: 'Number confirmation',
-    template: '<div>' + this.phone + '</div><div>Is this your phone number above correct?</div>',
-    cssClass: 'text-center',
-    okText: 'Yes',
-    okType: 'button-positive button-clear',
-    cancelText: 'edit',
-    cancelType: 'button-dark button-clear'
-  });
-
-  confirm.Popup.then((res) => {
-    if (!res) return;
-
-    $ionicLoading.show({
-      template: 'Sending verification code...'
+    let confirmPopup = $ionicPopup.confirm({
+      title: 'Number confirmation',
+      template: '<div>' + this.phone + '</div><div>Is this your phone number above correct?</div>',
+      cssClass: 'text-center',
+      okText: 'Yes',
+      okType: 'button-positive button-clear',
+      cancelText: 'edit',
+      cancelType: 'button-dark button-clear'
     });
 
-    Accounts.requestPhoneVerification(this.phone, (err) => {
-      $ionicLoading.hide();
+    confirm.Popup.then((res) => {
+      if (!res) return;
 
-      if (err) {
-        return handleError(err);
-      }
+      $ionicLoading.show({
+        template: 'Sending verification code...'
+      });
 
-      $state.go('confirmation', {phone: this.phone});
+      Accounts.requestPhoneVerification(this.phone, (err) => {
+        $ionicLoading.hide();
+
+        if (err) {
+          return handleError(err);
+        }
+
+        $state.go('confirmation', {phone: this.phone});
+      });
     });
-  });
-}
-
-
-
-
-
-
   }
+
+  function handleError(err) {
+    $log.error('Login error ', err);
+
+    $ionicPopup.alert({
+      title: err.reason || 'Login failed',
+      template: 'Please try again',
+      okType: 'button-positive button-clear'
+    });
+  }
+}
