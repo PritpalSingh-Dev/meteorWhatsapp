57af3a0f887370773cb2d8229495ad4d399fb7ec
psbadewal <psbadewal@icloud.com>
2016-03-13 22:06:54 +0000

    Implementing logout function inside SettingsCtrl function

--- whatsapp/client/scripts/controllers/settings.controller.js
+++ whatsapp/client/scripts/controllers/settings.controller.js
@@ -1,3 +1,15 @@
 angular
   .module('Whatsapp')
   .controller('SettingsCtrl', SettingsCtrl);
+
+  function SettingsCtrl($scope, $reactive, $state) {
+    $reactive(this).attach($scope);
+
+    this.logout = logout;
+
+    function logout(){
+      Meteor.logout((err) => {
+        $state.go('login');
+      });
+    }
+  }
