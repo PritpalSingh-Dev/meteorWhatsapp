ec35943291874e78f9ca12e691a0302223c30bb6
psbadewal <psbadewal@icloud.com>
2016-03-13 23:14:16 +0000

    Adding auto scroll when new messages arrive

--- whatsapp/client/scripts/controllers/chat.controller.js
+++ whatsapp/client/scripts/controllers/chat.controller.js
@@ -20,6 +20,11 @@ function ChatCtrl ($scope, $reactive, $stateParams, $ionicScrollDelegate, $timeo
     },
   });
 
+$scope.$watchCollection('chat.messages', (oldVal, newVal) => {
+  let animate = oldVal.length !== newVal.length;
+  $ionicScrollDelegate.$getByHandle('chatScroll').scrollBottom(animate);
+});
+
 ////////////
 
   function sendMessage() {
