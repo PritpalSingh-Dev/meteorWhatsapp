64d0b026dd35951fd08fbc7542f82a4894a376e9
psbadewal <psbadewal@icloud.com>
2016-03-13 19:25:45 +0000

    Enabling user to update name through ProfileCtrl

--- whatsapp/client/scripts/controllers/profile.controller.js
+++ whatsapp/client/scripts/controllers/profile.controller.js
@@ -1,3 +1,13 @@
 angular
   .module('Whatsapp')
   .controller('ProfileCtrl', ProfileCtrl);
+
+  function ProfileCtrl ($scope, $reactive, $state, $ionicPopup, $log) {
+    $reactive(this).attach($scope);
+
+    let user = Meteor.user();
+    let name = user && user.profile ? user.profile.name : '';
+
+    this.name = name;
+    this.updateName = updateName
+  }
