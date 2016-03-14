ce6e13e11d8f1712135c9cea62f26ee5d13266c1
psbadewal <psbadewal@icloud.com>
2016-03-14 23:52:27 +0000

    Adding hideNewChatModal, newChat and goToChat functions

--- whatsapp/client/scripts/controllers/new-chat.controller.js
+++ whatsapp/client/scripts/controllers/new-chat.controller.js
@@ -7,4 +7,29 @@ function NewChatCtrl($scope, $reactive, $state, NewChat) {
 
   this.hideNewChatModal = hideNewChatModal;
   this.newChat = newChat;
+
+
+  this.helpers({
+      users() {
+        return Meteor.users.find({ _id: { $ne: Meteor.userId() } });
+      }
+    });
+
+    function hideNewChatModal() {
+      NewChat.hideModal();
+    }
+
+    function newChat(userId) {
+      let chat = Chats.findOne({ type: 'chat', userIds: { $all: [Meteor.userId(), userId] } })
+      if (chat) {
+        return goToChat(chat._id);
+      }
+
+      Meteor.call('newChat', userId, goToChat);
+    }
+    
+    function goToChat(chatId) {
+      hideNewChatModal();
+      return $state.go('tab.chat', { chatId: chatId});
+    }
 }
